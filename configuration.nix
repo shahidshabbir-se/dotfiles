3ff1e55b38c7516e30ab01e2894e6bed6c8c4380
shahidshabbir-se <shahidshabbirse@gmail.com>
2025-01-16 09:59:22 +0500

    feat(app): neovim revamped,discord and hyprpanel config changed

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,81 @@
-/home/shahid/dotfiles/configuration.nix
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./modules/tmux.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  time.timeZone = "Asia/Karachi";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.blueman.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  services.libinput.enable = true;
+  services.upower.enable = true;
+
+  users.users.shahid = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "audio" ];
+    packages = with pkgs; [
+      hyprland
+      zsh
+      git
+      sddm
+      home-manager
+      discord
+    ];
+    shell = pkgs.zsh;
+  };
+
+  home-manager.backupFileExtension = "backup";
+  programs.hyprland.enable = true;
+  virtualisation.docker.enable = true;
+  programs.zsh.enable = true;
+
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = { };
+          otherlayer = { };
+        };
+        extraConfig = ''
+          [ids]
+          *
+
+          [main]
+          capslock = overload(control, esc)
+          esc = capslock
+        '';
+      };
+    };
+  };
+
+  system.stateVersion = "25.05";
+}
--- home.nix
+++ home.nix
@@ -1,1 +1,210 @@
-/home/shahid/dotfiles/home.nix
+{ pkgs, lib, inputs, nixpkgs, ... }:
+
+{
+  home.username = "shahid";
+  home.homeDirectory = "/home/shahid";
+
+
+  imports = [
+    inputs.spicetify-nix.homeManagerModules.default
+    inputs.hyprpanel.homeManagerModules.hyprpanel
+  ];
+
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+    {
+      enable = true;
+
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+        newReleases
+        ncsVisualizer
+      ];
+      enabledSnippets = with spicePkgs.snippets; [
+        rotatingCoverart
+        pointer
+      ];
+
+      theme = spicePkgs.themes.text;
+      colorScheme = "TokyoNight";
+    };
+
+
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  home.packages = with pkgs; [
+    kitty
+    tmux
+    neovim
+    git
+    swww
+    qbittorrent
+    conda
+    ntfs3g
+    cliphist
+    git-graph
+    prisma-engines
+    wl-clipboard
+    bat
+    keyd
+    yazi
+    brightnessctl
+    wget
+    wofi
+    btrfs-progs
+    zip
+    xz
+    spicetify-cli
+    eww
+    unzip
+    ripgrep
+    jq
+    eza
+    fzf
+    docker
+    zoxide
+    nodejs_23
+    btop
+    mtr
+    iperf3
+    nmap
+    ipcalc
+    gnused
+    gnutar
+    playerctl
+    gawk
+    zstd
+    gnupg
+    tree
+    noto-fonts-emoji
+    alacritty
+    kanshi
+    nodePackages.prisma
+    swaylock-effects
+    gcc
+    feh
+    nitch
+    stylua
+    rustup
+    nixpkgs-fmt
+    firefox
+
+    # hyprpanel dependencies
+    ags
+    wireplumber
+    libgtop
+    bluez
+    bluetui
+    dart-sass
+    upower
+    gvfs
+
+    # additional dependencies
+    # power-profiles-daemon
+    grimblast
+    gpu-screen-recorder
+    hyprpicker
+    hyprsunset
+    hypridle
+    cava
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "shahidshabbir-se";
+    userEmail = "shahidshabbirse@gmail.com";
+  };
+
+  programs.hyprpanel = {
+    enable = true;
+    systemd.enable = true;
+    overwrite.enable = true;
+    theme = "tokyo_night";
+    layout = {
+      "bar.layouts" = {
+        "*" = {
+          "left" = [
+            "dashboard"
+            "media"
+            "windowtitle"
+          ];
+          "middle" = [
+            "cava"
+            "workspaces"
+            "cava"
+          ];
+          "right" = [
+            "network"
+            "bluetooth"
+            "volume"
+            "battery"
+            "clock"
+            "power"
+          ];
+        };
+      };
+    };
+
+
+
+    settings = {
+      bar.workspaces.show_icons = true;
+      theme = {
+        font.size = "13px";
+        font.name = "JetBrainsMono Nerd Font";
+        bar = {
+          transparent = true;
+          outer_spacing = "0.4rem";
+        };
+      };
+      wallpaper.enable = false;
+      bar.bluetooth.label = false;
+      bar.network.label = false;
+      bar.clock.format = "%a %b %d %I:%M %p";
+      bar.media.show_active_only = false;
+      theme.bar.menus.menu.clock.scaling = 80;
+      theme.bar.menus.menu.dashboard.scaling = 80;
+      bar.media.truncation_size = 20;
+      bar.launcher.autoDetectIcon = true;
+      bar.workspaces.showApplicationIcons = false;
+      theme.bar.buttons.modules.power.spacing = "0";
+      bar.workspaces.monitorSpecific = false;
+      bar.workspaces.applicationIconEmptyWorkspace = "";
+      menus.clock = {
+        time = {
+          military = true;
+          hideSeconds = true;
+        };
+        weather = {
+          enabled = false;
+          location = "14000";
+          unit = "metric";
+        };
+      };
+    };
+  };
+
+  home.file = {
+    ".zshrc".source = ./.config/.zshrc;
+    ".p10k.zsh".source = ./.config/.p10k.zsh;
+    ".config/kitty".source = ./.config/kitty;
+    ".config/alacritty".source = ./.config/alacritty;
+    ".config/yazi".source = ./.config/yazi;
+    # ".config/nvim".source = ./.config/nvim;
+    ".local/share/fonts".source = ./.local/share/fonts;
+    # ".config/hypr".source = ./config/hypr;
+    ".config/bat".source = ./.config/bat;
+    ".config/swaylock".source = ./.config/swaylock;
+  };
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}
