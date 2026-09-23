fb90346ef1a471d67d01944e1e63930081da4705
shahidshabbir-se <shahidshabbirse@gmail.com>
2026-09-23 17:31:43 +0500

    chore(nvim): remove archived nvim2 configuration

--- config/archive/nvim2/lua/config/which-key.lua
+++
@@ -1,6 +0,0 @@
-require("which-key").setup({
-	preset = "modern",
-	win = {
-		border = "rounded",
-	},
-})
--- config/archive/nvim2/lua/keymaps/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.keymap.set
-
--- Toggle or focus the NvimTree file explorer
-map("n", "<leader>e", function()
-  require("nvim-tree.api").tree.toggle({
-    path = vim.loop.cwd(),
-    current_window = false,
-  })
-end, { desc = "File Explorer (floating)" })
